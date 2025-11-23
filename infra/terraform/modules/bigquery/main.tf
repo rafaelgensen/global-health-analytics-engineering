e74c1ecbbf341d9274a0c21cdf76c90815941fd4
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-23 04:12:11 -0300

    build: cloud functions + bq + schedules

--- infra/terraform/modules/bigquery/main.tf
+++ infra/terraform/modules/bigquery/main.tf
@@ -1,14 +1,14 @@
 resource "google_bigquery_dataset" "bronze" {
-  dataset_id = "global-health-analytics-engineering_23112025_bronze"
+  dataset_id = "global_health_analytics_engineering_23112025_bronze"
   location   = "us-central1"
 }
 
 resource "google_bigquery_dataset" "silver" {
-  dataset_id = "global-health-analytics-engineering_23112025_silver"
+  dataset_id = "global_health_analytics_engineering_23112025_silver"
   location   = "us-central1"
 }
 
 resource "google_bigquery_dataset" "gold" {
-  dataset_id = "global-health-analytics-engineering_23112025_gold"
+  dataset_id = "global_health_analytics_engineering_23112025_gold"
   location   = "us-central1"
 }
