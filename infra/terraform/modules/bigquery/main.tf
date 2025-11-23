1658b5c403c97c117751e05d9b5e09051ad539d5
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-11-23 20:29:35 -0300

    fix: scheduler and cloud functions connection

--- infra/terraform/modules/bigquery/main.tf
+++ infra/terraform/modules/bigquery/main.tf
@@ -3,6 +3,19 @@ resource "google_bigquery_dataset" "bronze" {
   location   = "us-central1"
 }
 
+resource "google_bigquery_table" "bronze_who_raw" {
+  dataset_id = google_bigquery_dataset.bronze.dataset_id
+  table_id   = "who_raw"
+
+  schema = jsonencode([
+    {
+      name = "raw"
+      type = "JSON"
+      mode = "NULLABLE"
+    }
+  ])
+}
+
 resource "google_bigquery_dataset" "silver" {
   dataset_id = "global_health_analytics_engineering_23112025_silver"
   location   = "us-central1"
